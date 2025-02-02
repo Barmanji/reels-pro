276716b1447adb32d0764bed5012bd6d08751586
Ajay Barman <barmanjiaj@gmail.com>
2025-02-02 13:57:21 +0530

    feat: User model add

---
+++ model/User.ts
@@ -0,0 +1,32 @@
+import mongoose, { model, Schema, models } from "mongoose"; //model for creating new, models for giving an array of all models
+import bcrypt from "bcrypt";
+
+export interface IUser{
+    email: string;
+    password: string;
+    _id?: mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+// <user> is the datetype of the User that we defined at the top, ITS TYPESCRIPT
+const userSchema = new Schema<IUser>({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, {timestamps: true})
+
+userSchema.pre("save", async function (next){  // pre hook runs just before saving userSchema
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+const User = models?.User || model<IUser>("User",userSchema) // model import because we dont have to write mongoose.model
+export default User;
---
+++ model/Video.ts
@@ -0,0 +1,37 @@
+import mongoose, { model, Schema, models } from "mongoose"; // model for creating new, models for giving an array of all models
+
+export const VIDEO_DIMENSION = {
+    width: 1080,
+    height: 1920,
+} as const // read what is as const?
+export interface IVideo{
+    _id?: mongoose.Types.ObjectId;
+    title: string;
+    description: string;
+    thumbnailUrl: string;
+    videoUrl: string;
+    controls?: boolean;
+    transformation?: {
+        height: number;
+        width: number;
+        quality?: number;
+    }
+    //duration: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const videoSchema = new Schema<IVideo>({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    videoUrl: {type: String, required: true},
+    thumbnailUrl: {type: String, required: true},
+    controls: {type: Boolean, default: true},
+    transformation: {
+        height: {type: Number, default: VIDEO_DIMENSION.height},
+        width: {type: Number, default: VIDEO_DIMENSION.width},
+        quality: {type: Number, min: 1, max: 100}
+    }
+}, {timestamps: true})
+
+
