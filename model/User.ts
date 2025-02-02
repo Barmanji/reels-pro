89a2a623d523e3d07dd8c385fd4f70e1da3c954a
Ajay Barman <barmanjiaj@gmail.com>
2025-02-02 23:21:11 +0530

    dummy commit, testing nvim format.lsp

--- model/User.ts
+++ model/User.ts
@@ -1,7 +1,7 @@
 import mongoose, { model, Schema, models } from "mongoose"; //model for creating new, models for giving an array of all models
 import bcrypt from "bcrypt";
 
-export interface IUser{
+export interface IUser {
     email: string;
     password: string;
     _id?: mongoose.Types.ObjectId;
@@ -19,14 +19,14 @@ const userSchema = new Schema<IUser>({
         type: String,
         required: true,
     }
-}, {timestamps: true})
+}, { timestamps: true })
 
-userSchema.pre("save", async function (next){  // pre hook runs just before saving userSchema
-    if(this.isModified("password")){
+userSchema.pre("save", async function(next) {  // pre hook runs just before saving userSchema
+    if (this.isModified("password")) {
         this.password = await bcrypt.hash(this.password, 10);
     }
     next();
 });
 
-const User = models?.User || model<IUser>("User",userSchema) // model import because we dont have to write mongoose.model
+const User = models?.User || model<IUser>("User", userSchema) // model import because we dont have to write mongoose.model
 export default User;
